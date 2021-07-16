5e1d8adb86e9d9560e765545fe72a0a02fc308d0
Siewlin Yap <siewlin@aisingapore.org>
2021-07-16 12:01:10 +0800

    tap out data input xformation for std metrics comparison on regression task

--- src/tenjin/interpreters/structured_data/int_general_metrics.py
+++ src/tenjin/interpreters/structured_data/int_general_metrics.py
@@ -4,30 +4,63 @@ Version :
 draft 3.0 [ 5 July 2021 - dash ]
 
 """
+import pandas as pd
+import numpy as np
+from sklearn import metrics
 
 
-class GeneralMetrics:
+ERR_DESC = ['Mean Absolute Error', 'Mean Squared Error', 'Root Mean Squared Error', 'R Squared']
+ERR_NAMES = ['MAE', 'MSE', 'RMSE', 'R2']
+METRICS_FORMULA = ['![MAE](assets/MAE.png)', '![MSE](assets/MSE.png)', '![RMSE](assets/RMSE.png)', '![R2](assets/R2.png)']
+ERR_METRICS_DICT = {}
+ERR_METRICS_DICT['Metrics Description'] = ERR_DESC
+ERR_METRICS_DICT['Formula'] = METRICS_FORMULA
+ERR_METRICS_DICT['Metrics Name'] = ERR_NAMES
+
+
+class IntGeneralMetrics:
     """
     - Transform raw data into input format suitable for plotting with plotly
     - General metrics cover confusion matrix, classification report, roc curve and precisionRecall curve
 
     Arguments:
         data_loader {class object} -- class object from data_loader pipeline
-        viz_plot {str} -- visualization type : 'confMat', 'classRpt', 'rocAUC' or 'preRacall' 
+        viz_plot {str} -- visualization type : 'confMat', 'classRpt', 'rocAUC', 'preRacall', 'stdErr', None
 
     Returns:
-        yTrue {pd.series} -- actual labels
-        yPred {list of list} -- Predicted labels of different models
-        model_names (list) -- name of models used to produce yPred
+        either (if analysis_type==regression)
+            yTrue {pd.series} -- actual labels
+            yPred {list of list} -- Predicted labels of different models
+            model_names {list} -- name of models used to produce yPred
+        or (if analysis_type==classification)
+            df {dataframe} -- dataframe used to plot fig-obj in visualizer module
         """
     def __init__(self, data_loader, viz_plot=None):
         self.data_loader = data_loader
         self.viz_plot = viz_plot
         self.analysis_type = self.data_loader.get_analysis_type()
 
+    def _standard_error_metrics(self, yTrue, yPred):
+        mae = metrics.mean_absolute_error(yTrue, yPred)
+        mse = metrics.mean_squared_error(yTrue, yPred)
+        rmse = np.sqrt(metrics.mean_squared_error(yTrue, yPred))
+        r2_score = metrics.r2_score(yTrue, yPred)
+        return [int(mae), int(mse), int(rmse), round(r2_score, 4)]
+
     def xform(self):
         if self.analysis_type == 'regression':
-            df = self.data_loader.get_all()
+            if self.viz_plot == 'stdErr':
+                yTrue = self.data_loader.get_yTrue()
+                yPreds = self.data_loader.get_yPreds()
+                models = self.data_loader.get_model_list()
+                if len(models)==1:
+                    ERR_METRICS_DICT[f'Model_{models[0]}'] = self._standard_error_metrics(yTrue, yPreds)
+                elif len(models)>1:
+                    for i in range(len(models)):
+                        ERR_METRICS_DICT[f'Model_{models[i]}'] = self._standard_error_metrics(yTrue, yPreds[yPreds.columns[i]])
+                df = pd.DataFrame(ERR_METRICS_DICT)
+            else:
+                df = self.data_loader.get_all()
             return df
 
         elif 'classification' in self.analysis_type:
