84c12e9f30ec7cb16477baf96588ab4230781dbd
Siewlin Yap <siewlin@aisingapore.org>
2021-07-21 21:06:41 +0800

    setup separate conftest under tests folder

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+# add this in the conftest.py under tests folder
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_setup_options():
+    options = Options()
+    options.add_argument('--no-sandbox')
+    return options
