42900c97a5ed9f5577d1698b78a74ea9685f5f6c
Siewlin Yap <siewlin@aisingapore.org>
2021-07-21 17:10:14 +0800

    overall linting updates per flake8

--- src/tenjin/features/feat_general_metrics.py
+++ src/tenjin/features/feat_general_metrics.py
@@ -1,23 +1,19 @@
-import dash
-from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import sys
 
 from ..interpreters.structured_data import IntGeneralMetrics
 from ..visualizer import general_metrics as viz_general
 
 
-
 def fig_confusion_matrix(data_loader):
     """
     Create confusion matrix
-    
+
     Arguments:
         data_loader {class object} 
         -- output from data loader pipeline 
-    
+
     Returns:
         plotly graph 
         -- displaying confusion matrix details
@@ -30,11 +26,11 @@ def fig_confusion_matrix(data_loader):
 def fig_classification_report(data_loader):
     """
     Create classification report in table form
-    
+
     Arguments:
         data_loader {class object} 
         -- output from data loader pipeline 
-    
+
     Returns:
         plotly table 
         -- containing classification report details
@@ -47,11 +43,11 @@ def fig_classification_report(data_loader):
 def fig_roc_curve(data_loader):
     """
     Display roc curve for comparison on various models
-    
+
     Arguments:
         data_loader {class object} 
         -- output from data loader pipeline 
-    
+
     Returns:
         plotly line curve 
         -- comparing roc-auc score for various models
@@ -64,11 +60,11 @@ def fig_roc_curve(data_loader):
 def fig_precisionRecall_curve(data_loader):
     """
     Display precision-recall curve for comparison on various models
-    
+
     Arguments:
         data_loader {class object} 
         -- output from data loader pipeline 
-    
+
     Returns:
         plotly line curve 
         -- comparing roc-auc score for various models
@@ -77,14 +73,15 @@ def fig_precisionRecall_curve(data_loader):
     fig_obj = viz_general.plot_precisionRecall_curve(yTrue, yPred, model_names)
     return fig_obj
 
+
 def fig_prediction_actual_comparison(data_loader):
     """
     Display comparison on prediction (yPred) vs actual (yTrue)
-    
+
     Arguments:
         data_loader {class object} 
         -- output from data loader pipeline 
-    
+
     Returns:
         plotly scatter plot
         -- comparing state of Prediction vs Actual for various models
@@ -93,14 +90,15 @@ def fig_prediction_actual_comparison(data_loader):
     fig_obj = viz_general.plot_prediction_vs_actual(df)
     return fig_obj
 
+
 def fig_prediction_offset_overview(data_loader):
     """
     Display overview of prediction offset
-    
+
     Arguments:
         data_loader {class object} 
         -- output from data loader pipeline 
-    
+
     Returns:
         plotly scatter plot with baseline
         -- comparing state of Prediction vs Residual / Offset for various models
@@ -109,14 +107,15 @@ def fig_prediction_offset_overview(data_loader):
     fig_obj = viz_general.plot_prediction_offset_overview(df)
     return fig_obj
 
+
 def fig_standard_error_metrics(data_loader):
     """
     Display table comparing various standard metrics for regression task
-    
+
     Arguments:
         data_loader {class object} 
         -- output from data loader pipeline 
-    
+
     Returns:
         dash table
         -- comparing general metrics covering MAE, MSE, RSME, R2 for single model and bimodal
@@ -147,18 +146,16 @@ class GeneralMetrics:
                                 dbc.Row([
                                     dbc.Col([
                                         dcc.Graph(figure=self.pred_actual),
-                                        ], className="border__common"), 
+                                    ], className="border__common"), 
 
                                     dbc.Col([
                                         dcc.Graph(figure=self.pred_offset),
-                                        ], className="border__common")
-                                    ]), 
+                                    ], className="border__common")
+                                ]), 
 
                                 html.Div(html.Div(self.std_error_metrics, className="div__std-err")),
                                 html.Br(),
-                                # html.Div(html.Div(dcc.Graph(figure=self.prec_recall), className="fig__roc-prec-recall"), className="border__common")
                         ], fluid=True)
-
 
         elif 'classification' in self.analysis_type:
             if len(self.conf_matrix) > 1:
@@ -167,16 +164,20 @@ class GeneralMetrics:
                                         dbc.Col([
                                             dcc.Graph(figure=self.conf_matrix[0]),
                                             dcc.Graph(figure=self.cls_report[0]),
-                                            ], className="border__common"), 
+                                        ], className="border__common"), 
 
                                         dbc.Col([
                                             dcc.Graph(figure=self.conf_matrix[1]),
                                             dcc.Graph(figure=self.cls_report[1]),
-                                            ], className="border__common")
-                                        ]), 
+                                        ], className="border__common")
+                                    ]), 
 
-                                    html.Div(html.Div(dcc.Graph(figure=self.roc), className="fig__roc-prec-recall"), className="border__common"),
-                                    html.Div(html.Div(dcc.Graph(figure=self.prec_recall), className="fig__roc-prec-recall"), className="border__common")
+                                    html.Div(html.Div(dcc.Graph(figure=self.roc), 
+                                                                className="fig__roc-prec-recall"), 
+                                                                className="border__common"),
+                                    html.Div(html.Div(dcc.Graph(figure=self.prec_recall), 
+                                                                className="fig__roc-prec-recall"), 
+                                                                className="border__common")
                             ], fluid=True)
 
             elif len(self.conf_matrix) == 1:
@@ -184,10 +185,14 @@ class GeneralMetrics:
                                     html.Div(dbc.Row([
                                         dbc.Col(dcc.Graph(figure=self.conf_matrix[0]), className="border__common"), 
                                         dbc.Col(dcc.Graph(figure=self.cls_report[0]), className="border__common")
-                                        ]), className="boundary__common"), 
+                                    ]), className="boundary__common"), 
 
-                                    html.Div(html.Div(dcc.Graph(figure=self.roc), className="fig__roc-prec-recall"), className="border__common"),
-                                    html.Div(html.Div(dcc.Graph(figure=self.prec_recall), className="fig__roc-prec-recall"), className="border__common")
+                                    html.Div(html.Div(dcc.Graph(figure=self.roc), 
+                                                                className="fig__roc-prec-recall"), 
+                                                                className="border__common"),
+                                    html.Div(html.Div(dcc.Graph(figure=self.prec_recall), 
+                                                                className="fig__roc-prec-recall"), 
+                                                                className="border__common")
                             ], fluid=True, className="boundary__common")
 
         return gen_metrics
