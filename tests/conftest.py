bed43536dfb3423c4db90072ef8a14672dba24b2
Siewlin Yap <siewlin@aisingapore.org>
2021-07-21 21:21:04 +0800

    add remark for future reference on Chrome crashed issue during integration test in gitlab-ci flow

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,11 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_setup_options():
     options = Options()
+
+    # added mainly for integration test in gitlab-ci to resolve 
+    # (unknown error: DevToolsActivePort file doesn't exist)
+    # (The process started from chrome location /usr/bin/google-chrome is no longer running, 
+    # so ChromeDriver is assuming that Chrome has crashed.)
+    # solution reference => https://github.com/plotly/dash/issues/1420
     options.add_argument('--no-sandbox')
     return options
