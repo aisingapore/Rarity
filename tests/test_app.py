8cca9870576471e18a7ec95fe4d4ad693b808ada
Siewlin Yap <siewlin@aisingapore.org>
2021-07-21 17:13:24 +0800

    adjustment for flake8 compliance

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,34 +1,36 @@
 from dash.testing.application_runners import import_app
-from tenjin.data_loader import CSVDataLoader
 from tenjin import GapAnalyzer
 
 
-def test_GapAnalyzer_single_modal_reg(dash_duo, csv_data_loader_single_modal_reg):
-    analyzer = GapAnalyzer(csv_data_loader_single_modal_reg, 'TestCase Analysis 1')
-    app = import_app('src.tenjin.app')
-    app.layout = analyzer._layout()
-    dash_duo.start_server(app)
-    assert dash_duo.find_element("#tabs-feature-page")
-    assert dash_duo.find_element("#feature-page")
- 
-def test_GapAnalyzer_bimodal_reg(dash_duo, csv_data_loader_bimodal_reg):
-    analyzer = GapAnalyzer(csv_data_loader_bimodal_reg, 'TestCase Analysis 2')
+def test_GapAnalyzer_single_modal_reg(dash_duo, csv_loader_single_modal_reg):
+    analyzer = GapAnalyzer(csv_loader_single_modal_reg, 'TestCase Analysis 1')
     app = import_app('src.tenjin.app')
     app.layout = analyzer._layout()
     dash_duo.start_server(app)
     assert dash_duo.find_element("#tabs-feature-page")
     assert dash_duo.find_element("#feature-page")
 
-def test_GapAnalyzer_single_modal_cls(dash_duo, csv_data_loader_single_modal_cls):
-    analyzer = GapAnalyzer(csv_data_loader_single_modal_cls, 'TestCase Analysis 3')
+
+def test_GapAnalyzer_bimodal_reg(dash_duo, csv_loader_bimodal_reg):
+    analyzer = GapAnalyzer(csv_loader_bimodal_reg, 'TestCase Analysis 2')
     app = import_app('src.tenjin.app')
     app.layout = analyzer._layout()
     dash_duo.start_server(app)
     assert dash_duo.find_element("#tabs-feature-page")
     assert dash_duo.find_element("#feature-page")
- 
-def test_GapAnalyzer_bimodal_cls(dash_duo, csv_data_loader_bimodal_cls):
-    analyzer = GapAnalyzer(csv_data_loader_bimodal_cls, 'TestCase Analysis 4')
+
+
+def test_GapAnalyzer_single_modal_cls(dash_duo, csv_loader_single_modal_cls):
+    analyzer = GapAnalyzer(csv_loader_single_modal_cls, 'TestCase Analysis 3')
+    app = import_app('src.tenjin.app')
+    app.layout = analyzer._layout()
+    dash_duo.start_server(app)
+    assert dash_duo.find_element("#tabs-feature-page")
+    assert dash_duo.find_element("#feature-page")
+
+
+def test_GapAnalyzer_bimodal_cls(dash_duo, csv_loader_bimodal_cls):
+    analyzer = GapAnalyzer(csv_loader_bimodal_cls, 'TestCase Analysis 4')
     app = import_app('src.tenjin.app')
     app.layout = analyzer._layout()
     dash_duo.start_server(app)
