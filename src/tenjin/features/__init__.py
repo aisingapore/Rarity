670813fdee9b3f21442a5319b572d0f8a7f504d3
Siewlin Yap <siewlin@aisingapore.org>
2021-07-26 10:28:07 +0800

    include new class MissPredictions

--- src/tenjin/features/__init__.py
+++ src/tenjin/features/__init__.py
@@ -1,4 +1,5 @@
 from .feat_general_metrics import GeneralMetrics
+from .feat_miss_predictions import MissPredictions
 
 
-__all__ = ['GeneralMetrics']
+__all__ = ['GeneralMetrics', 'MissPredictions']
