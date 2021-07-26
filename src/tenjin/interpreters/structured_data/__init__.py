a0cc34671dac9afa28490cc5915db3c6f95f90af
Siewlin Yap <siewlin@aisingapore.org>
2021-07-26 09:04:52 +0800

    update new class IntMissPredictions

--- src/tenjin/interpreters/structured_data/__init__.py
+++ src/tenjin/interpreters/structured_data/__init__.py
@@ -1,4 +1,5 @@
 from .int_general_metrics import IntGeneralMetrics
+from .int_miss_predictions import IntMissPredictions
 
 
-__all__ = ['IntGeneralMetrics']
+__all__ = ['IntGeneralMetrics', 'IntMissPredictions']
