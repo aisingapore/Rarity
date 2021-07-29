dab73a8833e6b735790e55e4bcbc4c6db6e093a4
Siewlin Yap <siewlin@aisingapore.org>
2021-07-29 14:32:19 +0800

    1.add responsive-table component 2.add callback function 3.add data conversion function

--- src/tenjin/features/feat_miss_predictions.py
+++ src/tenjin/features/feat_miss_predictions.py
@@ -1,9 +1,13 @@
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import dash_table
 
-from ..interpreters.structured_data import IntMissPredictions
-from ..visualizers import miss_predictions as viz_misspred
+from tenjin.app import app
+from tenjin.interpreters.structured_data import IntMissPredictions
+from tenjin.visualizers import miss_predictions as viz_misspred
 
 
 def fig_probabilities_spread_pattern(data_loader):
@@ -12,7 +16,7 @@ def fig_probabilities_spread_pattern(data_loader):
 
     Arguments:
         data_loader {class object} 
-        -- output from data loader pipeline 
+        -- output from data loader pipeline
 
     Returns:
         plotly graph 
@@ -40,7 +44,46 @@ def fig_probabilities_spread_pattern(data_loader):
 def fig_plot_prediction_offset_overview(data_loader):
     df = IntMissPredictions(data_loader).xform()
     fig_obj = viz_misspred.plot_prediction_offset_overview(df)
-    return fig_obj
+    return fig_obj, df
+
+
+def convert_filtered_data_to_df(relayout_data, df, models):
+    """convert raw data format from relayout selection range by user into the correct df fit for viz purpose
+
+    Arguments:
+        relayout_data {dict}: data containing selection range indices returned from plotly graph
+        df {pandas dataframe}: dataframe tap-out from interpreters pipeline
+        models {list}: model names defined by user during spin-up of Tenjin app
+
+    Returns:
+        pandas dataframe 
+        -- dataframe fit for the responsive table-graph filtering
+    """
+    try:
+        x_start_idx = int(relayout_data['xaxis.range[0]']) if relayout_data['xaxis.range[0]'] > 0 else 0
+        x_stop_idx = int(relayout_data['xaxis.range[1]']) if relayout_data['xaxis.range[1]'] < len(df) else len(df) - 1
+        y_start_idx = int(relayout_data['yaxis.range[0]'])
+        y_stop_idx = int(relayout_data['yaxis.range[1]'])
+        offset_cols = [col for col in df.columns if 'offset_' in col]
+
+        df_filtered = df.iloc[df.index[x_start_idx]:df.index[x_stop_idx]]
+
+        if len(offset_cols) == 1:
+            if offset_cols[0].replace('offset_', '') == models[0]:
+                df_final = df_filtered[(df_filtered[offset_cols[0]] >= y_start_idx) & (df_filtered[offset_cols[0]] <= y_stop_idx)]
+            else:
+                df_final = df_filtered[(df_filtered[offset_cols[1]] >= y_start_idx) & (df_filtered[offset_cols[1]] <= y_stop_idx)]
+
+        elif len(offset_cols) > 1:
+            df_final_m1 = df_filtered[(df_filtered[offset_cols[0]] >= y_start_idx) & (df_filtered[offset_cols[0]] <= y_stop_idx)]
+            df_final_m2 = df_filtered[(df_filtered[offset_cols[1]] >= y_start_idx) & (df_filtered[offset_cols[1]] <= y_stop_idx)]
+
+            final_filtered_idx = set(df_final_m1.index).union(set(df_final_m2.index))
+            df_final = df_filtered[df_filtered['index'].isin(list(final_filtered_idx))]
+        return df_final
+
+    except TypeError:  # during first startup when no filtered data applied
+        pass
 
 
 class MissPredictions:
@@ -50,7 +93,9 @@ class MissPredictions:
         self.is_bimodal = True if len(self.data_loader.get_model_list()) > 1 else False
 
         if self.analysis_type == 'regression':
-            self.preds_offset = fig_plot_prediction_offset_overview(self.data_loader)
+            self.preds_offset, self.df = fig_plot_prediction_offset_overview(self.data_loader)
+            self.cols_dash_table = [col.replace('_', ' ') for col in self.df.columns]
+
         elif 'classification' in self.analysis_type:
             self.probs_pattern, self.label_state = fig_probabilities_spread_pattern(self.data_loader)
 
@@ -59,22 +104,55 @@ class MissPredictions:
             miss_preds = dbc.Container([
                                 dbc.Row([
                                     dbc.Col([
-                                        dbc.Row(dcc.Graph(figure=self.preds_offset), justify="center")
+                                        dbc.Row(dcc.Graph(id='filter-datapoint', figure=self.preds_offset,), justify="center")
                                     ], className="border__common"), 
                                 ]), 
 
-                                html.Br(),
+                                html.Div(html.H6('Click and drag on the graph to select the range of data points \
+                                                to inspect feature values'), className="h6__dash-table-note"),
+
+                                html.Div(
+                                        dash_table.DataTable(
+                                            id='responsive-dash-table',
+                                            columns=[{'id': c, 'name': c} for c in self.cols_dash_table],
+                                            page_action='none',
+                                            fixed_rows={'headers': True},
+                                            # fixed_columns={'headers': True, 'data': 1},  # alighment will be out 
+                                            # - bug reported in dash repo
+                                            style_data={'whiteSpace': 'normal', 'height': 'auto'},
+                                            style_cell={'textAlign': 'center',
+                                                        'border': '1px solid rgb(229, 211, 197)',
+                                                        'font-family': 'Arial',
+                                                        'margin-bottom': '0',
+                                                        'whiteSpace': 'normal',
+                                                        'height': 'auto',
+                                                        'minWidth': '200px',
+                                                        'width': '200px',
+                                                        'maxWidth': '200px'},
+                                            style_header={'fontWeight': 'bold',
+                                                        'color': 'white',
+                                                        'backgroundColor': '#7e746d ',
+                                                        'border': '1px solid rgb(229, 211, 197)'},
+                                            style_table={'width': 1400,
+                                                        'height': 200,
+                                                        'margin': 'auto'},
+                                            export_format='csv'),
+                                        className="div__table-proba-spread"),
+
+                                html.Br()
                         ], fluid=True)
+
         elif 'classification' in self.analysis_type:
             fig_objs_model_1 = self.probs_pattern[0]
             tables_model_1 = self.label_state[0]
+
             if self.is_bimodal and 'classification' in self.analysis_type:  # cover bimodal_binary and bimodal_multiclass
                 fig_objs_model_2 = self.probs_pattern[1]
                 tables_model_2 = self.label_state[1]
 
                 dash_fig_ls = []
                 for i in range(0, len(fig_objs_model_1), 2):
-                    try:
+                    try:  # enabling the display of a pair of figures for better comparison view
                         fig_pair = dbc.Row([
                                         dbc.Col([dbc.Row([
                                                 dbc.Col([
@@ -109,7 +187,7 @@ class MissPredictions:
                                         ], className="border__common")])
 
                         dash_fig_ls.append(fig_pair)
-                    except IndexError:
+                    except IndexError:  # handling the last odd figure that can't be paired out
                         fig_pair = dbc.Row([
                                         dbc.Col([dbc.Row([
                                                 dbc.Col([
@@ -131,7 +209,7 @@ class MissPredictions:
 
                 miss_preds = dbc.Container(dash_fig_ls, fluid=True)
 
-            elif not self.is_bimodal and 'binary' in self.analysis_type:
+            elif not self.is_bimodal and 'binary' in self.analysis_type:  # single modal binary classification
                 miss_preds = dbc.Container([
                                     dbc.Row([
                                         dbc.Col([
@@ -159,7 +237,7 @@ class MissPredictions:
                                     dbc.Row([]),
                             ], fluid=True)
 
-            elif not self.is_bimodal and 'multiclass' in self.analysis_type:
+            elif not self.is_bimodal and 'multiclass' in self.analysis_type:  # single modal multi-class classification
                 dash_fig_ls = []
                 for i in range(0, len(fig_objs_model_1)):
                     fig_pair = dbc.Col([
@@ -175,3 +253,23 @@ class MissPredictions:
                                     dbc.Row(),
                             ], fluid=True)
         return miss_preds
+
+    def callback(self):
+        @app.callback(
+            Output('responsive-dash-table', 'data'), 
+            Input('filter-datapoint', 'relayoutData'))
+        def display_relayout_data(relayoutData):
+            if relayoutData is not None:
+                try:
+                    self.df = self.df.round(2)
+                    print(f'len_df_b4_dash_table: {len(self.df)}')
+                except TypeError:
+                    self.df
+
+                models = self.data_loader.get_model_list()
+                df_final = convert_filtered_data_to_df(relayoutData, self.df, models)
+                df_final.columns = self.cols_dash_table
+                data_relayout = df_final.to_dict('records')
+                return data_relayout
+            else:
+                raise PreventUpdate
