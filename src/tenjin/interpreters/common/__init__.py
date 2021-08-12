40d7350df0adb19ab7127181dd87af3318939542
Siewlin Yap <siewlin@aisingapore.org>
2021-08-12 11:25:10 +0800

    add new functions on logloss and optimum_num_cluster

--- src/tenjin/interpreters/common/__init__.py
+++ src/tenjin/interpreters/common/__init__.py
@@ -1,3 +1,5 @@
-from .calculation_logloss import create_clusters
+from .calculation_loss_clusters import create_clusters, calculate_logloss, find_optimum_num_clusters
 
-__all__ = ['create_clusters']
+__all__ = ['create_clusters',
+            'calculate_logloss',
+            'find_optimum_num_clusters']
