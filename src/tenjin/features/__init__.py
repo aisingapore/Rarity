cf20397cb1cf572c0de89a5b45b44a57cd9896d3
Siewlin Yap <siewlin@aisingapore.org>
2021-08-25 16:49:39 +0800

    add new class FeatureDistribution

--- src/tenjin/features/__init__.py
+++ src/tenjin/features/__init__.py
@@ -1,6 +1,10 @@
 from .feat_general_metrics import GeneralMetrics
 from .feat_miss_predictions import MissPredictions
 from .feat_loss_clusters import LossClusters
+from .feat_feature_distribution import FeatureDistribution
 
 
-__all__ = ['GeneralMetrics', 'MissPredictions', 'LossClusters']
+__all__ = ['GeneralMetrics',
+            'MissPredictions',
+            'LossClusters',
+            'FeatureDistribution']
