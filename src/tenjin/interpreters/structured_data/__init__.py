310dc52eacb0614a55112169c44ceebb58b6df7b
Siewlin Yap <siewlin@aisingapore.org>
2021-08-25 16:08:58 +0800

    add new class IntFeatureDistribution

--- src/tenjin/interpreters/structured_data/__init__.py
+++ src/tenjin/interpreters/structured_data/__init__.py
@@ -1,8 +1,10 @@
 from .int_general_metrics import IntGeneralMetrics
 from .int_miss_predictions import IntMissPredictions
 from .int_loss_clusters import IntLossClusterer
+from .int_xfeature_distribution import IntFeatureDistribution
 
 
 __all__ = ['IntGeneralMetrics',
             'IntMissPredictions',
-            'IntLossClusterer']
+            'IntLossClusterer',
+            'IntFeatureDistribution']
