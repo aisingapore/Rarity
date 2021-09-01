2abf85680218473ad3952ecdea54bb29cd47d00d
Siewlin Yap <siewlin@aisingapore.org>
2021-09-01 17:33:02 +0800

    include new class IntSimilaritiesCounterFactuals

--- src/tenjin/interpreters/structured_data/__init__.py
+++ src/tenjin/interpreters/structured_data/__init__.py
@@ -2,9 +2,11 @@ from .int_general_metrics import IntGeneralMetrics
 from .int_miss_predictions import IntMissPredictions
 from .int_loss_clusters import IntLossClusterer
 from .int_xfeature_distribution import IntFeatureDistribution
+from .int_similarities_counter_factuals import IntSimilaritiesCounterFactuals
 
 
 __all__ = ['IntGeneralMetrics',
             'IntMissPredictions',
             'IntLossClusterer',
-            'IntFeatureDistribution']
+            'IntFeatureDistribution',
+            'IntSimilaritiesCounterFactuals']
