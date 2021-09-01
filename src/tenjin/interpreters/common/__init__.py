3fdc07fcd14a29b1f1b840731c65e15452d0e279
Siewlin Yap <siewlin@aisingapore.org>
2021-09-01 17:18:16 +0800

    consolidate all calculations under utils.methods

--- src/tenjin/interpreters/common/__init__.py
+++
@@ -1,8 +0,0 @@
-from .calculation_loss_clusters import create_clusters, calculate_logloss, get_optimum_num_clusters
-from .calculate_kl_divergence import get_optimum_bin_size, calculate_kl_div
-
-__all__ = ['create_clusters',
-            'calculate_logloss',
-            'get_optimum_num_clusters',
-            'get_optimum_bin_size',
-            'calculate_kl_div']
