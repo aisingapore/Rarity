69325d753c19a917a30e6110d5400f900e00486f
Siewlin Yap <siewlin@aisingapore.org>
2021-09-01 13:17:44 +0800

    add default slider range and minor adjustment on default header style

--- src/tenjin/utils/style_configs.py
+++ src/tenjin/utils/style_configs.py
@@ -7,12 +7,14 @@ INSTRUCTION_TEXT_REG = 'To reset back to default settings, hover over icons on t
 WARNING_TEXT = 'To inspect new range of datapoints in different graph, please first reset the earlier selection by clicking "Reset axes" icon ' \
                 'at the top right corner of the graph.'
 
-DEFAULT_HEADER_STYLE = {'fontWeight': 'bold', 'color': 'white', 'backgroundColor': '#7e746d', 'border': '1px solid rgb(229, 211, 197)'}
+DEFAULT_HEADER_STYLE = {'fontWeight': 'bold', 'color': 'white', 'backgroundColor': '#7e746d', 'border': '1px solid white', 'height': '45px'}
 DEFAULT_TITLE_STYLE = {'visibility': 'visible'}
 DEFAULT_PLOT_NAME_STYLE = {'visibility': 'visible'}
 
 DEFAULT_RANGE_SELECTION_TEXT_REG = "Enter range of data to compare distribution ( default slicing - last 20% of dataset ) :"
 DEFAULT_RANGE_SELECTION_TEXT_CLS = "Enter range of data to compare distribution ( default slicing - full range ):"
+
+DEFAULT_SLIDER_RANGE = {n: str(n) for n in range(1, 11)}
 
 
 def default_header_style():
