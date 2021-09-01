4d61e12a0524dddd0b9ad22b6ef050dea7ced9c8
Siewlin Yap <siewlin@aisingapore.org>
2021-09-01 15:23:47 +0800

    add new function for similaritiesCF and conditional formating on dash table

--- src/tenjin/visualizers/shared_viz_component.py
+++ src/tenjin/visualizers/shared_viz_component.py
@@ -7,8 +7,8 @@ def reponsive_table_to_filtered_datapoints(data, customized_cols, header, exp_fo
                     columns=[{'id': c, 'name': c} for c in customized_cols],
                     page_action='none',
                     fixed_rows={'headers': True},
-                    # fixed_columns={'headers': True, 'data': 1},  # alighment will be out
-                    # - bug reported in dash repo
+                    fixed_columns={'headers': True, 'data': 1},
+                    style_header=header,
                     style_data={'whiteSpace': 'normal', 'height': 'auto'},
                     style_cell={'textAlign': 'center',
                                 'border': '1px solid rgb(229, 211, 197)',
@@ -19,9 +19,56 @@ def reponsive_table_to_filtered_datapoints(data, customized_cols, header, exp_fo
                                 'minWidth': '200px',
                                 'width': '200px',
                                 'maxWidth': '200px'},
-                    style_header=header,
                     style_table={'width': 1400,
                                 'height': 200,
                                 'margin': 'auto'},
+                    export_format=exp_format),
+    return table_obj
+
+
+def reponsive_table_to_filtered_datapoints_similaritiesCF(df, customized_cols, feature_cols, header, exp_format):
+    data = df.to_dict('records')
+    table_obj = dash_table.DataTable(
+                    data=data,
+                    columns=[{'id': c, 'name': c} for c in customized_cols],
+                    page_action='none',
+                    fixed_rows={'headers': True},
+                    fixed_columns={'headers': True, 'data': 1},
+                    style_header=header,
+                    style_data={'whiteSpace': 'normal', 'height': 'auto'},
+                    style_cell={'textAlign': 'center',
+                                'border': '1px solid rgb(229, 211, 197)',
+                                'font-family': 'Arial',
+                                'margin-bottom': '0',
+                                'whiteSpace': 'normal',
+                                'height': 'auto',
+                                'minWidth': '200px',
+                                'width': '200px',
+                                'maxWidth': '200px'},
+                    style_table={'width': 1400,
+                                'height': 200,
+                                'margin': 'auto'},
+                    style_header_conditional=[
+                        {
+                            'if': {'column_id': col},
+                            'backgroundColor': '#b69e8d',
+                            'border': '1px solid white',
+                        } for col in feature_cols
+                    ],
+                    style_data_conditional=[
+                        {
+                            'if': {
+                                'column_id': 'category',
+                            },
+                            'border-top': '1px solid rgb(229, 211, 197)',
+                            'fontWeight': 'bold'}
+                    ] + [
+                        {
+                            'if': {
+                                'filter_query': '{category} = "User_defined_idx"',
+                            },
+                            'backgroundColor': 'rgb(229, 240, 247)',
+                        }
+                    ],
                     export_format=exp_format),
     return table_obj
