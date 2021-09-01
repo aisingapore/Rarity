bc671bc8aa0e36ba1372747ae9e2a67d3cf6d4fb
Siewlin Yap <siewlin@aisingapore.org>
2021-09-01 17:14:00 +0800

    adopt common functions related to the conditional application to reg or cls

--- src/tenjin/features/feat_feature_distribution.py
+++ src/tenjin/features/feat_feature_distribution.py
@@ -9,17 +9,18 @@ from tenjin.app import app
 from tenjin.interpreters.structured_data import IntFeatureDistribution
 from tenjin.visualizers.xfeature_distribution import plot_distribution_by_specific_feature, plot_distribution_by_kl_div_ranking
 from tenjin.utils import style_configs
-from tenjin.utils.common_functions import detected_bimodal, invalid_slicing_range, invalid_slicing_range_sequence, \
-                                            invalid_min_max_limit, incomplete_range_entry
+from tenjin.utils.common_functions import is_regression, is_classification, detected_bimodal, \
+                                            invalid_slicing_range, invalid_slicing_range_sequence, invalid_min_max_limit, \
+                                            incomplete_range_entry, get_max_value_on_slider
 
 
 def fig_plot_distribution_by_kl_div_ranking(data_loader, feature_to_exclude, start_idx, stop_idx, display_option, display_value):
-    if data_loader.analysis_type == 'regression':
+    if is_regression(data_loader.analysis_type):
         kl_div_dict_sorted = IntFeatureDistribution(data_loader).xform(feature_to_exclude, start_idx, stop_idx)
         fig_obj_dict = plot_distribution_by_kl_div_ranking(kl_div_dict_sorted, display_option, display_value, 'dataset_type', None)
         return fig_obj_dict
 
-    elif 'classification' in data_loader.analysis_type:
+    elif is_classification(data_loader.analysis_type):
         models = data_loader.get_model_list()
         ls_kl_div_dict_sorted = IntFeatureDistribution(data_loader).xform(feature_to_exclude, start_idx, stop_idx)
         ls_fig_obj_dict = [plot_distribution_by_kl_div_ranking(kl_div_dict, display_option, display_value, 'pred_state', models[i])
@@ -28,12 +29,12 @@ def fig_plot_distribution_by_kl_div_ranking(data_loader, feature_to_exclude, sta
 
 
 def fig_plot_distribution_by_specific_feature(data_loader, ls_specific_feature, start_idx, stop_idx):
-    if data_loader.analysis_type == 'regression':
+    if is_regression(data_loader.analysis_type):
         kl_div_dict_sorted = IntFeatureDistribution(data_loader).xform(None, start_idx, stop_idx)
         fig_objs_specific = plot_distribution_by_specific_feature(ls_specific_feature, kl_div_dict_sorted, 'dataset_type', None)
         return fig_objs_specific
 
-    elif 'classification' in data_loader.analysis_type:
+    elif is_classification(data_loader.analysis_type):
         models = data_loader.get_model_list()
         ls_kl_div_dict_sorted = IntFeatureDistribution(data_loader).xform(None, start_idx, stop_idx)
         ls_fig_objs_specific = [plot_distribution_by_specific_feature(ls_specific_feature, kl_div_dict, 'pred_state', models[i])
@@ -169,13 +170,6 @@ def _second_level_validity_check_on_user_input_range(range_list, df):
     return range_input_err_alert
 
 
-def _get_max_value_on_slider(df_features):
-    max_value = len(list(df_features.columns))
-    if len(list(df_features.columns)) > 10:
-        max_value = 10
-    return max_value
-
-
 def _generate_valid_start_stop_idx_based_on_usr_input_range(slicing_input, df_features, default_start_idx, default_stop_idx):
     if slicing_input is not None:
         if invalid_slicing_range(slicing_input):
@@ -229,14 +223,16 @@ class FeatureDistribution:
         self.specific_feature = []
         self.display_option = 'top'
         self.display_value = 3
-        if self.analysis_type == 'regression':
+
+        if is_regression(self.analysis_type):
             self.start_idx = int(len(self.df_features) * 0.8)
             self.stop_idx = len(self.df_features)
             self.range_selection_header = style_configs.DEFAULT_RANGE_SELECTION_TEXT_REG
-        elif 'classification' in self.analysis_type:
+        elif is_classification(self.analysis_type):
             self.start_idx = None
             self.stop_idx = None
             self.range_selection_header = style_configs.DEFAULT_RANGE_SELECTION_TEXT_CLS
+
         self.fig_obj_dict = fig_plot_distribution_by_kl_div_ranking(self.data_loader,
                                                                     self.feature_to_exclude,
                                                                     self.start_idx,
@@ -250,19 +246,18 @@ class FeatureDistribution:
 
     def show(self):
         options_feature_ls_reg = [{'label': f'{col}', 'value': f'{col}'} for col in self.df_features.columns]
-        slider_range_topN_bottomN = {n: str(n) for n in range(1, 11)}
 
-        if self.analysis_type == 'regression' or (('classification' in self.analysis_type) and not detected_bimodal(self.model_names)):
+        if is_regression(self.analysis_type) or (is_classification(self.analysis_type) and not detected_bimodal(self.model_names)):
             if isinstance(self.fig_obj_dict, list):
                 self.fig_obj_dict = self.fig_obj_dict[0]
-            if 'classification' in self.analysis_type:
+            if is_classification(self.analysis_type):
                 self.fig_objs_specific = self.fig_objs_specific[0]
 
             self.fig_objs_display_combined = _fig_objs_layout_based_on_display_option(self.fig_obj_dict, self.display_option)
             self.fig_objs_specific_feature_combined = _fig_objs_layout_based_on_specific_feature(self.specific_feature,
                                                                                                     self.fig_objs_specific)
 
-        elif ('classification' in self.analysis_type) and detected_bimodal(self.model_names):
+        elif is_classification(self.analysis_type) and detected_bimodal(self.model_names):
             self.fig_objs_display_combined = _model_pair_fig_objs_layout_on_display_option(self.fig_obj_dict, self.display_option)
             self.fig_objs_specific_feature_combined = _model_pair_fig_objs_layout_on_specific_feature(self.specific_feature,
                                                                                                         self.fig_objs_specific)
@@ -278,6 +273,7 @@ class FeatureDistribution:
                         dbc.Col(dcc.Dropdown(id='select-feature-to-exclude-featdist',
                                             options=options_feature_ls_reg,
                                         value=[], multi=True)),
+                        html.Br(),
                         html.Br(),
                         dbc.Row([
                             dbc.Col([
@@ -295,10 +291,10 @@ class FeatureDistribution:
                                                 inline=True, inputClassName='radiobutton__select-top-bottom-n', custom=False)])),
                                 dcc.Slider(id='select-slider-top-bottom-range-featdist',
                                             min=1,
-                                            max=_get_max_value_on_slider(self.df_features),  # max at 10
+                                            max=get_max_value_on_slider(self.df_features, 'feat-dist'),  # max at 10
                                             step=1,
                                             value=3,
-                                            marks=slider_range_topN_bottomN)], width=6),
+                                            marks=style_configs.DEFAULT_SLIDER_RANGE)], width=6),
                             dbc.Col([
                                 dbc.Row(html.Div(html.H6(self.range_selection_header), className='h6__enter-data-range')),
                                 dbc.Row(dbc.Input(id='input-range-to-slice-kldiv-featdist',
@@ -308,7 +304,6 @@ class FeatureDistribution:
                                 dbc.Row(html.Div(html.Pre(style_configs.input_range_subnote(self.df_features),
                                                             className='text__range-header-kldiv-featfist')))],
                                 width=6)]),
-
                         html.Br(),
                         dbc.Row([
                             dbc.Col(width=10),
@@ -391,13 +386,13 @@ class FeatureDistribution:
                                                                             stop_idx,
                                                                             display_option,
                                                                             display_value)
-                    if self.analysis_type == 'regression' or \
-                            (('classification' in self.analysis_type) and not detected_bimodal(self.model_names)):
+                    if is_regression(self.analysis_type) or \
+                            (is_classification(self.analysis_type) and not detected_bimodal(self.model_names)):
                         if isinstance(fig_obj_dict, list):
                             fig_obj_dict = fig_obj_dict[0]
                         fig_objs_display_combined = _fig_objs_layout_based_on_display_option(fig_obj_dict, display_option)
 
-                    elif ('classification' in self.analysis_type) and detected_bimodal(self.model_names):
+                    elif is_classification(self.analysis_type) and detected_bimodal(self.model_names):
                         fig_objs_display_combined = _model_pair_fig_objs_layout_on_display_option(fig_obj_dict, display_option)
                     return '', range_input_err_alert, fig_objs_display_combined
                 else:
@@ -424,13 +419,13 @@ class FeatureDistribution:
                     start_idx, stop_idx = compact_outputs_specific_feat[0], compact_outputs_specific_feat[1]
                     fig_objs_specific = fig_plot_distribution_by_specific_feature(self.data_loader, specific_feature, start_idx, stop_idx)
 
-                    if (self.analysis_type == 'regression') or \
-                            (('classification' in self.analysis_type) and not detected_bimodal(self.model_names)):
-                        if 'classification' in self.analysis_type:
+                    if is_regression(self.analysis_type) or \
+                            (is_classification(self.analysis_type) and not detected_bimodal(self.model_names)):
+                        if is_classification(self.analysis_type):
                             fig_objs_specific = fig_objs_specific[0]
                         fig_objs_specific_feature_combined = _fig_objs_layout_based_on_specific_feature(specific_feature, fig_objs_specific)
 
-                    elif ('classification' in self.analysis_type) and detected_bimodal(self.model_names):
+                    elif is_classification(self.analysis_type) and detected_bimodal(self.model_names):
                         fig_objs_specific_feature_combined = _model_pair_fig_objs_layout_on_specific_feature(specific_feature, fig_objs_specific)
                     return '', range_input_err_alert, fig_objs_specific_feature_combined
                 else:
