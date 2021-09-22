b092eae27bdec13c7f51261d9ad0dfdb220020c3
Siewlin Yap <siewlin@aisingapore.org>
2021-09-23 07:36:51 +0800

    rename affected tenjin api to rarity

--- src/tenjin/__init__.py
+++
@@ -1,3 +0,0 @@
-from .analyzer import GapAnalyzer
-
-__all__ = ['GapAnalyzer']
--- src/tenjin/app.py
+++
@@ -1,8 +0,0 @@
-import dash
-
-
-app = dash.Dash(__name__, 
-                meta_tags=[{'name': 'viewport', 
-                            'content': 'width=device-width, initial-scale=1.0'}], 
-                suppress_callback_exceptions=True)
-server = app.server
