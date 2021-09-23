f7ba6ed6aa1ad9ee1135e7e5538add590602ff72
Siewlin Yap <siewlin@aisingapore.org>
2021-09-23 10:44:43 +0800

    update import to rarity

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,10 @@
 from dash.testing.application_runners import import_app
-from tenjin import GapAnalyzer
+from rarity import GapAnalyzer
 
 
 def test_GapAnalyzer_single_modal_reg(dash_duo, csv_loader_single_modal_reg):
     analyzer = GapAnalyzer(csv_loader_single_modal_reg, 'TestCase Analysis 1')
-    app = import_app('tenjin.app')
+    app = import_app('rarity.app')
     app.layout = analyzer._layout()
     dash_duo.start_server(app)
     assert dash_duo.find_element("#tabs-feature-page")
@@ -13,7 +13,7 @@ def test_GapAnalyzer_single_modal_reg(dash_duo, csv_loader_single_modal_reg):
 
 def test_GapAnalyzer_bimodal_reg(dash_duo, csv_loader_bimodal_reg):
     analyzer = GapAnalyzer(csv_loader_bimodal_reg, 'TestCase Analysis 2')
-    app = import_app('tenjin.app')
+    app = import_app('rarity.app')
     app.layout = analyzer._layout()
     dash_duo.start_server(app)
     assert dash_duo.find_element("#tabs-feature-page")
@@ -22,7 +22,7 @@ def test_GapAnalyzer_bimodal_reg(dash_duo, csv_loader_bimodal_reg):
 
 def test_GapAnalyzer_single_modal_cls(dash_duo, csv_loader_single_modal_cls):
     analyzer = GapAnalyzer(csv_loader_single_modal_cls, 'TestCase Analysis 3')
-    app = import_app('tenjin.app')
+    app = import_app('rarity.app')
     app.layout = analyzer._layout()
     dash_duo.start_server(app)
     assert dash_duo.find_element("#tabs-feature-page")
@@ -31,7 +31,7 @@ def test_GapAnalyzer_single_modal_cls(dash_duo, csv_loader_single_modal_cls):
 
 def test_GapAnalyzer_bimodal_cls(dash_duo, csv_loader_bimodal_cls):
     analyzer = GapAnalyzer(csv_loader_bimodal_cls, 'TestCase Analysis 4')
-    app = import_app('tenjin.app')
+    app = import_app('rarity.app')
     app.layout = analyzer._layout()
     dash_duo.start_server(app)
     assert dash_duo.find_element("#tabs-feature-page")
