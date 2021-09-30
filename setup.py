534e10ecdc5e368c3db6cb01da5ee4fbde18a6c8
SiewLinYap <siewlin2009@gmail.com>
2021-09-30 15:41:17 +0800

    update setup configs

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+# Copyright 2021 AI Singapore. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from setuptools import setup, find_packages
 
 
@@ -16,26 +30,30 @@ setup(
     name='rarity',
     version='1.0.dev0',
     description='Data diagnostic package with minimum setup to analyze miss predictions of ML models',
-    keywords='data-analysis model-prediction dianogstic',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    keywords='data analysis model prediction diagnostic',
     author='Yap Siew Lin, Jeanne Choo, Chong Wei Yih @ AI Singapore',
     author_email='siewlin@aisingapore.org',
-    license="Apache 2.0",
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    entry_points={"console_scripts": ["rarity-cli=rarity.commands.rarity_cli:main"]},
-    python_requires=">=3.6.0",
+    license='Apache 2.0',
+    url='https://github.com/aimakerspace/Rarity',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    package_data={'rarity': ['py.typed']},
+    entry_points={'console_scripts': ['rarity-cli=rarity.commands.rarity_cli:main']},
+    python_requires='>=3.6.0',
     install_requires=REQUIRED_PKGS,
-    extras_require={"notebook": ["jupyter-client>=6.0.0", "jupyter-core>=4.6.3", "ipywidgets>=7.5.1"]},
+    extras_require={'notebook': ['jupyter-client>=6.0.0', 'jupyter-core>=4.6.3', 'ipywidgets>=7.5.1']},
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
 )
